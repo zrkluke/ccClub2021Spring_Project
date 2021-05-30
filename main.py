84ba1f3cbe8099ce3210822375f6a3cada2d1e0b
zrkluke <zrkong0309@gmail.com>
2021-05-31 01:01:15 +0800

    新增中文查詢功能

--- main.py
+++ main.py
@@ -18,17 +18,24 @@ def hello():
 @app.route('/stock/')
 def stock_query():
     stock_id = request.args.get('stock_id')
+    stock_name = type(stock_id)
+    table = pd.read_csv('./database/stock_id.csv')
+    if stock_id.isdigit():
+        if int(stock_id) in table['股票代號'].values:
+            stock_name = table[table['股票代號'] == int(stock_id)]['名稱'].values[0]
+        else:
+            return render_template('stock.html')
+    elif stock_id.isalpha():
+        if stock_id in table['名稱'].values:
+            stock_name = stock_id
+            stock_id = str(table[table['名稱'] == stock_id]['股票代號'].values[0])
+        else:
+            return render_template('stock.html')
+    else:
+        pass
+        
     df = pd.read_csv('.\\database\\' + stock_id + '.csv')
-    if stock_id == '1101':
-        stock_name = '台泥'
-    elif stock_id == '1234':
-        stock_name = '黑松'
-    elif stock_id == '2345':
-        stock_name = '智邦'
-    elif stock_id == '2330':
-        stock_name = '台積電'
-    elif stock_id == '8464':
-        stock_name = '億豐'
+ 
     column_names = df.columns.tolist()
     df = df.astype({'年度':'object'})
     df = df.values.tolist()

b0cf3e38d265ad552a082cec24da17220f92ffee
zrkluke <zrkong0309@gmail.com>
2021-06-11 02:39:41 +0800

    新增股價估價功能

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from requests.models import parse_header_links
 from config import DevConfig
 import pandas as pd
 import os
-from goodinfo_crawler import *
+import time
 
 # 初始化 Flask 類別成為 instance
 app = Flask(__name__)
@@ -27,26 +27,44 @@ def stock_query():
         if int(stock_id) in table['股票代號'].values:
             stock_name = table[table['股票代號'] == int(stock_id)]['名稱'].values[0]
         else:
-            return render_template('search.html') # 此股票不存在
+            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看') # 此股票不存在
 
-    elif stock_id.isalpha():
-
+    else:
         if stock_id in table['名稱'].values:
             stock_name = stock_id
             stock_id = str(table[table['名稱'] == stock_id]['股票代號'].values[0])
         else:
-            return render_template('search.html') # 此股票不存在
+            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看') # 此股票不存在
 
-    else:
-        return render_template('search.html') # 此股票不存在
-        
-    df = pd.read_csv('.\\database\\' + stock_id + '_year.csv')
-    column_names = df.columns.tolist()
-    df = df.astype({'年度':'object'})
-    df = df.values.tolist()
 
-    return render_template('search.html', stock_name = stock_name, stock_id = stock_id, 
-                            column_names = column_names, df = df)
+    year_data_time = time.strftime("%Y/%m/%d",time.localtime(os.path.getmtime('.\\database\\' + stock_id + '_year.csv')))      
+    df_year = pd.read_csv('.\\database\\' + stock_id + '_year.csv')
+    column_year = df_year.columns.tolist()
+    df_year = df_year.astype({'年度':'object'})
+    year_data = df_year.values.tolist()
+
+    quarter_data_time = time.strftime("%Y/%m/%d",time.localtime(os.path.getmtime('.\\database\\' + stock_id + '_quarter.csv'))) 
+    df_quarter = pd.read_csv('.\\database\\' + stock_id + '_quarter.csv')
+    column_quarter = df_quarter.columns.tolist()
+    quarter_data = df_quarter.values.tolist()
+
+    df_year['RR'] = df_year['現金股利'] / df_year['每股稅後盈餘(EPS)']
+    ROA = df_year['資產報酬率(ROA)'].head(5).mean() / 100
+    RR = df_year['RR'].head().mean()
+    Growth = ROA * RR
+    EPS = df_year['每股稅後盈餘(EPS)'].head(5).mean()
+    D = EPS * (1 - RR)
+    
+    price_GGM = 123213
+    price_SPM = 31232
+
+    return render_template('search.html', stock_name = stock_name, stock_id = '(' + stock_id + ')', 
+                            column_year = column_year, year_data = year_data, year_data_time = year_data_time,
+                            column_quarter = column_quarter, quarter_data = quarter_data, quarter_data_time = quarter_data_time,
+                            Growth = Growth, EPS = EPS, D = D, price_GGM = price_GGM, price_SPM = price_SPM,
+                            )
+
+
 
 
 

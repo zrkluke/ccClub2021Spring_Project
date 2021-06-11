9594082855d58c491839b0a511cb52959211ba57
zrkluke <zrkong0309@gmail.com>
2021-06-11 21:58:08 +0800

    新增draw.py main_page.html

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # main.py
 
 from flask import Flask, request, render_template, url_for, redirect
-from requests.models import parse_header_links
 from config import DevConfig
 import pandas as pd
 import os
 import time
+import draw
 
 # 初始化 Flask 類別成為 instance
 app = Flask(__name__)
@@ -15,26 +15,25 @@ app.config.from_object(DevConfig)
 @app.route('/')
 @app.route('/index')
 def hello():
-    return render_template('mainpage.html', name = 'ccClub')
+    lst = draw.go()   
+    return render_template('mainpage.html', name = 'ccClub',lst=lst)
 
 @app.route('/stock/')
 def stock_query():
     stock_id = request.args.get('stock_id')
-    stock_name = type(stock_id)
     table = pd.read_csv('./database/stock_id.csv')
     if stock_id.isdigit():
-
         if int(stock_id) in table['股票代號'].values:
             stock_name = table[table['股票代號'] == int(stock_id)]['名稱'].values[0]
         else:
-            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看') # 此股票不存在
+            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看', Growth = 0) # 此股票不存在
 
     else:
         if stock_id in table['名稱'].values:
             stock_name = stock_id
             stock_id = str(table[table['名稱'] == stock_id]['股票代號'].values[0])
         else:
-            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看') # 此股票不存在
+            return render_template('search.html', stock_name = '您查詢的公司不存在', result = '請確認輸入的股票名稱或代號是否正確，再重新查詢看看', Growth = 0) # 此股票不存在
 
 
     year_data_time = time.strftime("%Y/%m/%d",time.localtime(os.path.getmtime('.\\database\\' + stock_id + '_year.csv')))      
@@ -55,13 +54,11 @@ def stock_query():
     EPS = df_year['每股稅後盈餘(EPS)'].head(5).mean()
     D = EPS * (1 - RR)
     
-    price_GGM = 123213
-    price_SPM = 31232
 
     return render_template('search.html', stock_name = stock_name, stock_id = '(' + stock_id + ')', 
                             column_year = column_year, year_data = year_data, year_data_time = year_data_time,
                             column_quarter = column_quarter, quarter_data = quarter_data, quarter_data_time = quarter_data_time,
-                            Growth = Growth, EPS = EPS, D = D, price_GGM = price_GGM, price_SPM = price_SPM,
+                            Growth = Growth, EPS = EPS, D = D
                             )
 
 
